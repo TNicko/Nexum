28f4303ba7d02d863980675111cdfeb5a6c44367
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2023-12-05 19:47:59 +0000

    added root handler

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ use axum::{
 async fn main() {
 
     // Build app with single route
-    let app = Router::new().route("/", get(|| async {"Hello, world!"}));
+    let app = Router::new()
+        .route("/", get(root));
 
     // Run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+async fn root() {}
