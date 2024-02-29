48aa1d564fa1ec2e46f951ad265ba01ba56c1db6
HordeOfRats <93047164+HordeOfRats@users.noreply.github.com>
2024-02-29 14:05:43 +0000

    optimised system function and removed unneccasary function

--- app/gpt/greet_student.py
+++ app/gpt/greet_student.py
@@ -69,60 +69,14 @@ def doGPTRequest(query, givenDocuments):
     llm = ChatOpenAI(openai_api_key="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", temperature=0, model_name="gpt-3.5-turbo-0125")
     output_parser = StrOutputParser()
     prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a bot designed to parse information from several chunks of text from websites, giving an answer to a given question"),
+        ("system", "You are a bot designed to parse information from several chunks of text from websites, giving an answer to a given question. You must only use information from the chunks themselves and not come up with any information on your own. If the answer isn't found within the chunks, reply with 'Sorry, this is outside the scope of my information' rather than making up an answer."),
         ("user", "{input}")
     ])
     chain = prompt | llm | output_parser
 
     return(chain.invoke({"input": "Question: "+query+" Information: "+givenDocuments}))
 
-def langchainRetrieveQuery():
-    load_dotenv()
 
-    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-    embeddings = OpenAIEmbeddings()
-
-    vstore = SupabaseVectorStore(
-        embedding=embeddings,
-        client=supabase,
-        table_name="documents",
-        query_name="match_documents",
-    )
-    metadata_field_info = [
-        AttributeInfo(
-            name="genre",
-            description="The genre of the movie",
-            type="string or list[string]",
-        ),
-        AttributeInfo(
-            name="year",
-            description="The year the movie was released",
-            type="integer",
-        ),
-        AttributeInfo(
-            name="director",
-            description="The name of the movie director",
-            type="string",
-        ),
-        AttributeInfo(
-            name="rating", description="A 1-10 rating for the movie", type="float"
-        ),
-    ]
-    document_content_description = "A chunk of text from a one of Loughborough Universities websites"
-    llm = OpenAI(temperature=0)
-    retriever = SelfQueryRetriever.from_llm(
-        llm,
-        vstore,
-        document_content_description,
-        metadata_field_info,
-        enable_limit=True,
-        verbose=True,
-    )
-    retriever.get_relevant_documents("what are two movies about dinosaurs")
-
-question = "What modules are one the masters computer science course?"
+question = "Who is the current head of the university?"
 docsToGive = (getMatched(question))
 print(doGPTRequest(question, docsToGive))
