e162e22aefdfdbe9136da7a1667651c4863118a6
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2024-03-14 19:43:38 +0000

    data response streaming (+dummy pipeline) implemented for api/query endpoint

--- app/api.py
+++ app/api.py
@@ -1,14 +1,20 @@
+import asyncio
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Query, UploadFile, Form
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, StreamingResponse
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage
 from pydantic import BaseModel
 from app.db.supabase import create_supabase_client
 
 app = FastAPI()
 
+from typing import AsyncIterable, Awaitable
+
 supabase = create_supabase_client()
 
-origins = ["http://localhost:5173", "localhost:5173"]
+origins = ["http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,18 +25,41 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def root() -> dict:
-    return {"message": "hello world!"}
-
-
-class CreateMessage(BaseModel):
-    id: int
+class CreateQuery(BaseModel):
     message: str
 
+async def send_message(message: str) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
+    model = ChatOpenAI(
+        streaming=True,
+        verbose=True,
+        callbacks=[callback]
+    )
+    
+    async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        """Wrap an awaitable with an event to signal when it's done or an exception is raised."""
+        try:
+            await fn
+        except Exception as e:
+            # TODO: handle exception
+            print(f"Caught exception: {e}")
+        finally:
+            # Signal the aiter to stop
+            event.set()
+
+    # Begin a task that runs in the background
+    task = asyncio.create_task(wrap_done(
+        model.agenerate(messages=[[HumanMessage(content=message)]]),
+        callback.done),
+    )
+
+    async for token in callback.aiter():
+        yield f"data: {token}\n\n"
+
+    await task
 
 @app.post("/api/query")
-async def query(request: CreateMessage) -> CreateMessage:
-    print(request)
-
-    return request
+def stream(body: CreateQuery):
+    return StreamingResponse(
+        send_message(body.message), media_type="text/event-stream"
+    )
