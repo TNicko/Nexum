cdc032c94976070b25b93f77f716367697b415d4
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2024-03-21 16:11:20 +0000

    fixed cors origin issue (added hosts)

--- server/app/api.py
+++ server/app/api.py
@@ -14,7 +14,7 @@ from typing import AsyncIterable, Awaitable
 
 supabase = create_supabase_client()
 
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://0.0.0.0:5173"]
 
 app.add_middleware(
     CORSMiddleware,
