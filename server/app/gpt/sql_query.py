23d57d8591ef6cd9acc33d1031059282a199763a
MikeMartin2 <94862053+MikeMartin2@users.noreply.github.com>
2024-04-20 19:25:27 +0100

    Updated Sql_query to integrate example selector

--- server/app/gpt/sql_query.py
+++ server/app/gpt/sql_query.py
@@ -8,6 +8,13 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langchain_openai import OpenAIEmbeddings
 from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    FewShotPromptTemplate,
+    MessagesPlaceholder,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+)
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,29 +31,43 @@ def getNonVectorizedData(message):
     llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
     examples = [
-        {"input": "Give me more information about Shakespeare Society.", "query": "SELECT * FROM Socities WHERE Name='Shakespeare';"},
-        {"input": "Give me more information about Skate Society.", "query": "SELECT * FROM Socities WHERE Name='Skate Socieity';"},
-        {
-            "input": "List all tracks that are longer than 5 minutes.",
-            "query": "SELECT * FROM Track WHERE Milliseconds > 300000;",
-        },
-        
+    {"input": "Give me more information about Shakespeare Society.", "query": "SELECT * FROM Socities WHERE Name='Shakespeare';"},
+    {"input": "Give me more information about Skate Society.", "query": "SELECT * FROM Socities WHERE Name='Skate Socieity';"},
+    {
+        "input": "List all tracks that are longer than 5 minutes.",
+        "query": "SELECT * FROM Track WHERE Milliseconds > 300000;",
+    },
+    
     ]
 
     example_selector = SemanticSimilarityExampleSelector.from_examples(
         examples,
         OpenAIEmbeddings(),
         FAISS,
-        k=1, 
+        k=1, #this should be increased when we have more examples
         input_keys=["input"],
     )
 
-    from langchain_core.prompts import (
-        ChatPromptTemplate,
-        FewShotPromptTemplate,
-        MessagesPlaceholder,
-        PromptTemplate,
-        SystemMessagePromptTemplate,
+
+    def query_as_list(db, query):
+        res = db.run(query)
+        res = [el for sub in ast.literal_eval(res) for el in sub if el]
+        res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+        return list(set(res))
+
+    ##Change this if needed
+    societies = query_as_list(db, "SELECT Name FROM Societies")
+
+    from langchain.agents.agent_toolkits import create_retriever_tool
+
+    vector_db = FAISS.from_texts(societies, OpenAIEmbeddings())
+    retriever = vector_db.as_retriever(search_kwargs={"k": 5})
+    description = """Use to look up values to filter on. Input is an approximate spelling of the proper noun, output is \
+    valid proper nouns. Use the noun most similar to the search."""
+    retriever_tool = create_retriever_tool(
+        retriever,
+        name="search_proper_nouns",
+        description=description,
     )
 
     system_prefix = """You are an agent designed to interact with a SQL database.
@@ -60,9 +81,14 @@ def getNonVectorizedData(message):
 
     DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
-    If the question does not seem related to the database, just return "Sorry, I dont know the answer to this." as the answer.
+    If the question does not seem related to the database, just return "I don't know" as the answer.
+
+    If you need to filter on a proper noun, you must ALWAYS first look up the filter value using the "search_proper_nouns" tool! 
+
+    You have access to the following tables: {table_names}
 
     Here are some examples of user inputs and their corresponding SQL queries:"""
+
 
     few_shot_prompt = FewShotPromptTemplate(
         example_selector=example_selector,
@@ -82,69 +108,24 @@ def getNonVectorizedData(message):
         ]
     )
 
+    """
     prompt_val = full_prompt.invoke(
         {
             "input": message,
             "top_k": 5,
             "dialect": "SQLite",
+            "table_names": "Societies",
             "agent_scratchpad": [],
         }
     )
-
-    agent = create_sql_agent(
-        llm=llm,
-        db=db,
-        prompt=full_prompt,
-        verbose=True,
-        agent_type="openai-tools",
-    )
-
-    def query_as_list(db, query):
-        res = db.run(query)
-        res = [el for sub in ast.literal_eval(res) for el in sub if el]
-        res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
-        return list(set(res))
-
-    # Change this if needed
-    societies = query_as_list(db, "SELECT Name FROM Societies")
-    
-    vector_db = FAISS.from_texts(societies, OpenAIEmbeddings())
-    retriever = vector_db.as_retriever(search_kwargs={"k": 5})
-    description = """Use to look up values to filter on. Input is an approximate spelling of the proper noun, output is \
-    valid proper nouns. Use the noun most similar to the search."""
-    retriever_tool = create_retriever_tool(
-        retriever,
-        name="search_proper_nouns",
-        description=description,
-    )
-
-    system = """You are an agent designed to interact with a SQL database.
-    Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-    Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-    You can order the results by a relevant column to return the most interesting examples in the database.
-    Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-    You have access to tools for interacting with the database.
-    Only use the given tools. Only use the information returned by the tools to construct your final answer.
-    You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
-
-    DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
-
-    If you need to filter on a proper noun, you must ALWAYS first look up the filter value using the "search_proper_nouns" tool! 
-
-    If the question does not seem related to the database, just return 'Sorry, I dont know the answer to this.' as the answer.
-
-    You have access to the following tables: {table_names}"""
-
-    prompt = ChatPromptTemplate.from_messages(
-        [("system", system), ("human", "{input}"), MessagesPlaceholder("agent_scratchpad")]
-    )
+    """
     agent = create_sql_agent(
         llm=llm,
         db=db,
         extra_tools=[retriever_tool],
-        prompt=prompt,
+        prompt=full_prompt,
         agent_type="openai-tools",
-        verbose=False, 
+        verbose=True,
     )
 
     response = agent.invoke({"input": message})
